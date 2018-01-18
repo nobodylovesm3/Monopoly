935548b8bc7274f977672dbba297751137e7beee
nobodylovesm3 <inc0n3ck@abv.bg>
2018-01-18 22:35:06 +0200

    added all global variables

--- src/PlayGame.java
+++ src/PlayGame.java
@@ -2,23 +2,61 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class PlayGame {
+
+    public static final int numberOfBoardSectors = 40;
+    public static final int BoardProperties = 7;
+    public static String[] rawInformationBoard = new String[numberOfBoardSectors];
+    public static String[][] readyBoard = new String[numberOfBoardSectors][BoardProperties];
+
+    public static final int numberCards = 5;
+    public static final int cardsProperties = 4;
+
+    public static String[] emptyCommunityCards = new String[numberCards];
+    public static String[][] readyCommunityCards = new String[numberCards][cardsProperties];
+
+    public static String[] emptyLuckyCards = new String[numberCards];
+    public static String[][] readyLuckyCards = new String[numberCards][cardsProperties];
+
+    public static final int maxAmountOfPlayers = 8;
+    public static final int minAmountOfPlayers = 2;
+    public static int playersAmount;
+    public static String[] playersNames = new String[maxAmountOfPlayers];
+    public static final int startingMoney = 1500;
+    public static int[] playersMoney = {startingMoney, startingMoney, startingMoney, startingMoney, startingMoney, startingMoney, startingMoney, startingMoney};
+
+    public static int cardNumber;
+    public static String cardName;
+    public static String cardAction;
+    public static String cardLoseWinMoney;
+    public static String cardChangePlayerPosition;
+
+    public static char choice;
+
+    public static final int defaultStartPosition = 0;
+    public static final int maxRoundsInJail = 3;
+    public static final int maxTimesDiceThrows = 3;
+
+    public static int allTurns;
+    public static int[] firstDiceThrows = new int[maxAmountOfPlayers];
+    public static int[] secondDiceThrows = new int[maxAmountOfPlayers];
+    public static int[] countDicePairsThrownInRow = {defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition};
+
+    public static String sectorAction;
+    public static String sectorOwnership;
+    public static String sectorCost;
+    public static String sectorBuildingsCounter;
+
+    public static boolean isReady = false;
+    public static boolean hasWon = false;
+
+    public static boolean[] isBankrupted = new boolean[maxAmountOfPlayers];
+    public static int numberBankruptedPlayers = defaultStartPosition;
+
+    public static boolean[] hasGetOutOfJailCard = new boolean[maxAmountOfPlayers];
+    public static int[] spentRoundsInJail = {defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition, defaultStartPosition};
+    public static boolean[] isInJail = new boolean[maxAmountOfPlayers];
+
     public static void main(String[] args) {
-
-        //boardCreation
-        int[] board = new int[40];
-        for (int i = 0; i < board.length; i++) {
-            board[i] = i + 1;
-        }
-
-        //namesPositions
-        String[] names = {"Go!", "Old Kent Road", "Community Chest", "Whitechapel Road", "INCOME TAX", "Kings Cross Station", "The Angel Islington", "CHANCE", "Euston Road", "Pentonville Road", "Visiting JAIL", "Pall Mall", "Electric Company", "Whitehall", "Northumrl'd Avenue", "Marylebone Station", "Bow street", "Community Shest", "Marlborough Street", "Vine Street", "Free Parking", "Strand", "CHANCE", "Fleet Street", "Trafalgar Square", "Fenchurch St. Station", "Leicester Square", "Coventry Street", "Water Works", "Piccadilly", "JAIL", "Regent Street", "Oxford Street", "Community Chest", "Bond Street", "Liverpool St. Station", "CHANCE", "Park Lane", "SUPER TAX", "Mayfair"};
-
-        //haveToCreateCards
-
-        //buySector
-        boolean[] numberOfBoughtSectors = new boolean[board.length];
-
-        int[] playerPosition = new int[numberOfPlayers()];
 
     }
 
@@ -30,79 +68,6 @@ public class PlayGame {
 
         return rand.nextInt(diceMaxValue - diceMinValue + 1) + diceMinValue;
     }
-
-    //getNumberOfPlayers
-    public static int numberOfPlayers() {
-        Scanner input = new Scanner(System.in);
-        int numberOfPlayers;
-        while (true) {
-            try {
-                do {
-                    numberOfPlayers = Integer.parseInt(input.nextLine());
-                } while (numberOfPlayers < 2 || numberOfPlayers > 8);
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("2 TO 8");
-                continue;
-            }
-        }
-        return numberOfPlayers;
-    }
-
-    public static boolean buySector(int index, boolean[] numberOfBoughtSectors) {
-        numberOfBoughtSectors[index] = true;
-        return numberOfBoughtSectors[index];
-    }
-
-    //    public static void movePlayers(int[] playerPosition, int[] board) {
-//        return movePlayer();
-//    }
-
-    public static void receiveOwnership(int index, boolean[] numberOfSectors) {
-        Scanner input = new Scanner(System.in);
-        String answer;
-        if (!isBought(index, numberOfSectors)) {
-            while (true) {
-                answer = input.nextLine();
-                if (answer.equalsIgnoreCase("yes")) {
-                    buySector(index, numberOfSectors);
-                    break;
-                } else if (answer.equalsIgnoreCase("no")) {
-                    break;
-                }
-            }
-        }
-    }
-
-    public static boolean isBought(int index, boolean[] numberOfBoughtSectors) {
-        return numberOfBoughtSectors[index];
-    }
-
-    public static int startMoney() {
-        return 2 * 500 + 2 * 100 + 2 * 50 + 6 * 20 + 10 * 5 + 5 * 5 + 1 * 5;
-    }
-
-    //notready
-    public static int countMoney(int[] playerPosition, int index, boolean[] numberOfBoughtSectors) {
-        int currentMoney = startMoney();
-
-        if (isBought(index, numberOfBoughtSectors) && playerPosition[index] == (index - 1)) {
-        //    currentMoney -= (index + 1) *;
-        }
-    }
-
-    public static int taxAmountSectors(int[] board, int index) {
-    }
-
-//    public static boolean isBrokeOut() {
-//        return countMoney() < 0;
-//    }
-
-//    public static void playGame(int[] board, String[] names, int[] players) {
-//        while (true) {
-//
-//        }
-//    }
 
 
 }
