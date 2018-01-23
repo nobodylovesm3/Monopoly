8c61f78ba1f37f6423eddfce0054ca1f9ea28889
nobodylovesm3 <inc0n3ck@abv.bg>
2018-01-24 00:56:33 +0200

    working partially - no movement and auction

--- src/PlayGame.java
+++ src/PlayGame.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class PlayGame {
 
+    public static Random rand = new Random();
+    public static Scanner input = new Scanner(System.in);
+
     public static final int numberOfBoardSectors = 40;
     public static final int boardProperties = 7;
     public static String[] rawInformationBoard = new String[numberOfBoardSectors];
@@ -44,6 +47,7 @@ public class PlayGame {
     public static String sectorAction;
     public static String sectorOwnership;
     public static String sectorCost;
+    public static String sectorColor;
     public static String sectorBuildingsCounter;
 
     public static boolean isReady = false;
@@ -57,9 +61,12 @@ public class PlayGame {
     public static boolean[] isInJail = new boolean[maxAmountOfPlayers];
 
     public static void main(String[] args) {
-
+        prepareGameBoardInformation();
+        inputPlayersNumber();
+        int[] playerLocation = new int[playersAmount];
+        inputPlayersNames();
+        startGame(playerLocation);
     }
-    //filledAllProperties
 
     public static void fillProperties() {
         fillBrownProperties();
@@ -137,15 +144,15 @@ public class PlayGame {
     }
 
     public static void fillCommunityChests() {
-        rawInformationBoard[1] = "COMMUNITY CHEST, chest, draw, -, -, -,0";
-        rawInformationBoard[17] = "COMMUNITY CHEST, chest, draw, -, -, -,0";
-        rawInformationBoard[33] = "COMMUNITY CHEST, chest, draw, -, -, -,0";
+        rawInformationBoard[1] = "COMMUNITY CHEST, chest, draw community chest card, -, -, -,0";
+        rawInformationBoard[17] = "COMMUNITY CHEST, chest, draw community chest card, -, -, -,0";
+        rawInformationBoard[33] = "COMMUNITY CHEST, chest, draw community chest card, -, -, -,0";
     }
 
     public static void fillChances() {
-        rawInformationBoard[7] = "CHANCE, chance, draw, -, -, -,0";
-        rawInformationBoard[22] = "CHANCE, chance, draw, -, -, -,0";
-        rawInformationBoard[36] = "CHANCE, chance, draw, -, -, -,0";
+        rawInformationBoard[7] = "CHANCE, chance, draw chance card, -, -, -,0";
+        rawInformationBoard[22] = "CHANCE, chance, draw chance card, -, -, -,0";
+        rawInformationBoard[36] = "CHANCE, chance, draw chance card, -, -, -,0";
     }
 
     public static void fillStart() {
@@ -211,5 +218,443 @@ public class PlayGame {
         fillTaxes();
         fillParkPlace();
     }
-    //to here
+
+    //players
+
+    public static void inputPlayersNumber() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of players: (max 8, min 2) ");
+        int numberOfPlayers = 0;
+
+        while (true) {
+            try {
+                numberOfPlayers = Integer.parseInt(input.nextLine());
+                if (numberOfPlayers > 2 && numberOfPlayers < 9) {
+                    break;
+                } else {
+                    System.out.println("Max players - 8, min players - 2. ");
+                }
+            } catch (Exception e) {
+                System.out.println("Max players - 8, min players - 2. ");
+                continue;
+            }
+        }
+
+        System.out.println();
+    }
+
+    public static void inputPlayersNames() {
+        System.out.println("Enter your names.");
+        for (int i = 0; i < playersAmount; i++) {
+            System.out.println("Player " + (i + 1) + " : ");
+            playersNames[i] = input.nextLine();
+            System.out.println();
+        }
+    }
+
+
+    public static int diceRoll() {
+        return rand.nextInt(6) + 1;
+    }
+
+    public static void outputResultDiceOne(int index) {
+        System.out.println("You rolled (first dice): " + firstDiceThrows[index]);
+    }
+
+    public static void outputResultDiceTwo(int index) {
+        System.out.println("You rolled (second dice): " + secondDiceThrows[index]);
+    }
+
+    public static boolean hasMadeFullLap(int[] playerLocation, int i) {
+        return playerLocation[i] + firstDiceThrows[i] + secondDiceThrows[i] >= numberOfBoardSectors;
+    }
+
+    public static void outputPlayerCurrentLocation(int[] playerLocation, int i) {
+        System.out.println("Location: " + playerLocation[i] + " " + readyBoard[playerLocation[i]][0] + ", " + readyBoard[playerLocation[i]][1] + ", " + readyBoard[playerLocation[i]][2] + ", " + readyBoard[playerLocation[i]][3] + ", " + readyBoard[playerLocation[i]][4] + ", " + readyBoard[playerLocation[i]][5] + ", " + readyBoard[playerLocation[i]][6]);
+    }
+
+    public static boolean isSectordOwned() {
+        return !sectorOwnership.equals("no owner");
+    }
+
+    public static boolean isSectorPurchasable() {
+        return sectorAction.equals("buy");
+    }
+
+    public static boolean isGoingToJail() {
+        return sectorAction.equals("imprison");
+    }
+
+    public static void getCurrentSectorInformation(int[] playerLocation, int i) {
+        sectorAction = readyBoard[playerLocation[i]][2];
+        sectorCost = readyBoard[playerLocation[i]][3];
+        sectorOwnership = readyBoard[playerLocation[i]][4];
+        sectorColor = readyBoard[playerLocation[i]][5];
+        sectorBuildingsCounter = readyBoard[playerLocation[i]][6];
+    }
+
+    public static void updateIrrelevantSectorsInformation(int[] playerLocation, int i) {
+        if (!sectorOwnership.equals("-") && !sectorCost.equals("-")) {
+            readyBoard[playerLocation[i]][3] = sectorCost;
+            readyBoard[playerLocation[i]][4] = sectorOwnership;
+        }
+    }
+
+    public static void drawCommunityChestCards() {
+        cardNumber = rand.nextInt(4);
+        cardName = readyCommunityCards[cardNumber][0];
+        cardAction = readyCommunityCards[cardNumber][1];
+        cardLoseWinMoney = readyCommunityCards[cardNumber][2];
+        cardChangePlayerPosition = readyCommunityCards[cardNumber][3];
+    }
+
+    public static void drawChanceCards() {
+        cardNumber = rand.nextInt(4);
+        cardName = readyChanceCards[cardNumber][0];
+        cardAction = readyChanceCards[cardNumber][1];
+        cardLoseWinMoney = readyChanceCards[cardNumber][2];
+        cardChangePlayerPosition = readyChanceCards[cardNumber][3];
+    }
+
+    public static void winMoneyCard(int i) {
+        if (cardAction.equals("win")) {
+            System.out.println("You won " + cardLoseWinMoney + "$.");
+            playersMoney[i] += Integer.parseInt(cardLoseWinMoney);
+        }
+    }
+
+
+    public static void loseMoneyCard(int i) {
+        if (cardAction.equals("pay")) {
+            System.out.println("You lost " + cardLoseWinMoney + "$.");
+            playersMoney[i] -= Integer.parseInt(cardLoseWinMoney);
+        }
+    }
+
+    public static void escapeOutOfJailCardDraw(int i) {
+        if (cardAction.equals("escape")) {
+            hasGetOutOfJailCard[i] = true;
+        }
+    }
+
+    public static void payMoneyOutput(int i) {
+        if (sectorAction.equals("pay")) {
+            System.out.println(playersNames[i] + " paid " + sectorCost + "$.");
+            playersMoney[i] -= Integer.parseInt(sectorCost);
+        }
+    }
+
+    public static void winMoneyOutput(int i) {
+        if (sectorAction.equals("win")) {
+            System.out.println(playersNames[i] + " won " + sectorCost + "$.");
+            playersMoney[i] += Integer.parseInt(sectorCost);
+        }
+    }
+
+    private static void rowTheDices(int i) {
+        System.out.println("Press enter to row the dice: ");
+        String readString = null;
+        readString = input.nextLine();
+        while (readString != null) {
+            if (readString.isEmpty()) {
+                int hello = 3;
+                System.out.println(hello);
+                break;
+            }
+
+            if (input.hasNextLine()) {
+                readString = input.nextLine();
+            } else {
+                readString = null;
+            }
+        }
+    }
+
+    public static void checkBankruptedPlayer(int i) {
+        if (playersMoney[i] < 0) {
+            if (!isBankrupted[i]) {
+                numberBankruptedPlayers++;
+            }
+            isBankrupted[i] = true;
+        }
+
+        if (isBankrupted[i]) {
+            System.out.println(playersNames[i] + "'s bankrupted and cannot play anymore.");
+            System.out.println();
+        }
+    }
+
+    public static boolean checkWhetherAllPlayersHaveBankrupted() {
+        boolean isEveryPlayerBankrupted = true;
+        for (int i = 0; i < playersAmount; i++) {
+            if (playersMoney[i] >= 0) {
+                isEveryPlayerBankrupted = false;
+                break;
+            }
+        }
+
+        return isEveryPlayerBankrupted;
+    }
+
+    public static void calculatePositionAfterThrownDice(int[] playerLocation, int i) {
+        if (!hasMadeFullLap(playerLocation, i)) {
+            playerLocation[i] += firstDiceThrows[i] + secondDiceThrows[i];
+        } else {
+            playerLocation[i] = (-1) * (numberOfBoardSectors - (playerLocation[i] + firstDiceThrows[i] + secondDiceThrows[i]));
+        }
+    }
+
+    public static void printMoney(int i) {
+        System.out.println("Money: " + playersMoney[i] + "$.");
+    }
+
+    public static void prepareGameBoardInformation() {
+        fillTheWholeBoardWithInformation();
+        formatBoard();
+
+        fillCommunityCardsInformation();
+        formatCommunityCards();
+
+        fillChanceCardsInformation();
+        formatChanceCards();
+    }
+
+    public static void isOnlyOnePlayerLeft(int i) {
+        if (playersAmount - numberBankruptedPlayers <= 1) {
+            for (int index = 0; index < playersAmount; index++) {
+                if (!isBankrupted[index]) {
+                    System.out.println("Congrats! " + playersNames[index] + " has won the game!");
+                    break;
+                }
+            }
+            System.out.println();
+            hasWon = true;
+        }
+    }
+
+    public static void proceedWithTurns(int[] playerLocation, int i) {
+        calculatePositionAfterThrownDice(playerLocation, i);
+        outputPlayerCurrentLocation(playerLocation, i);
+        printMoney(i);
+        decideWhatToDo(playerLocation, i);
+    }
+
+    public static void goToJail(int[] playerLocation, int i) {
+        System.out.println(playersNames[i] + " has been in jail for " + spentRoundsInJail[i] + " turns.");
+        System.out.println("You can: (1) try to throw a pair; \n(2) bribe the guard with 100$;\n(3) wait it out(released after " + (3 - spentRoundsInJail[i]) + " turns);\n(4) use a 'Get Out Of Jail' card if you have one.");
+
+        int decision;
+        do {
+            while (true) {
+                try {
+                    decision = Integer.parseInt(input.nextLine());
+                    if (decision > 0) {
+                        break;
+                    } else {
+                        System.out.println("Input either 1, 2, 3 or 4 (option).");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Input either 1, 2, 3 or 4 (option).");
+                    continue;
+                }
+            }
+            switch (decision) {
+                case 1: {
+                    rowTheDices(i);
+                    if (firstDiceThrows[i] == secondDiceThrows[i]) {
+                        System.out.println(playersNames[i] + " threw a pair and successfully escaped!");
+                        isInJail[i] = false;
+                        spentRoundsInJail[i] = 0;
+                        proceedWithTurns(playerLocation, i);
+                    } else {
+                        spentRoundsInJail[i]++;
+                    }
+                }
+                break;
+                case 2: {
+                    if (playersMoney[i] >= 100) {
+                        System.out.println(playersNames[i] + " successfully bribed the guard and escaped!");
+                        playersMoney[i] -= 100;
+                        isInJail[i] = false;
+                        spentRoundsInJail[i] = 0;
+
+                        rowTheDices(i);
+
+                        //check for pair
+                        proceedWithTurns(playerLocation, i);
+                    } else {
+                        System.out.println(playersNames[i] + " doesn't have enough money. ");
+                        spentRoundsInJail[i]++;
+                    }
+                    break;
+                }
+
+                case 3: {
+                    spentRoundsInJail[i]++;
+                    break;
+                }
+                case 4: {
+                    if (hasGetOutOfJailCard[i]) {
+                        System.out.println(playersNames[i] + " used the 'Get Out Of Jail' card and escaped!");
+                        isInJail[i] = false;
+                        spentRoundsInJail[i] = 0;
+                        hasGetOutOfJailCard[i] = false;
+
+                        rowTheDices(i);
+
+                        proceedWithTurns(playerLocation, i);
+                    } else {
+                        System.out.println(playersNames[i] + " doesn't have the 'Get Out Of Jail Card'.");
+                        spentRoundsInJail[i]++;
+                    }
+                    break;
+                }
+            }
+
+        }
+        while (decision != 1 && (decision != 2 && playersMoney[i] < 100) && decision != 3 && (decision != 4 && !hasGetOutOfJailCard[i]));
+
+        playerLocation[i] = 10;
+
+        if (spentRoundsInJail[i] >= maxRoundsInJail) {
+            isInJail[i] = false;
+            spentRoundsInJail[i] = 0;
+
+            rowTheDices(i);
+
+            proceedWithTurns(playerLocation, i);
+        }
+    }
+
+    private static void decideWhatToDo(int[] playerLocation, int i) {
+        getCurrentSectorInformation(playerLocation, i);
+
+        if (isGoingToJail() || isInJail[i]) {
+            goToJail(playerLocation, i);
+        }
+
+        if (!isInJail[i]) {
+            if (sectorAction.equals("draw community chest card")) {
+                drawCommunityChestCards();
+
+                System.out.println(playersNames[i] + " drew the " + cardName + " card.");
+
+                winMoneyCard(i);
+                loseMoneyCard(i);
+                escapeOutOfJailCardDraw(i);
+                positionChangeCardDraw(playerLocation, i);
+            }
+
+            if (sectorAction.equals("draw chance card")) {
+                drawChanceCards();
+
+                System.out.println(playersNames[i] + " drew the " + cardName + " card.");
+
+                winMoneyCard(i);
+                loseMoneyCard(i);
+                escapeOutOfJailCardDraw(i);
+                positionChangeCardDraw(playerLocation, i);
+            }
+        }
+
+        if (!isInJail[i]) {
+            winMoneyOutput(i);
+
+            payMoneyOutput(i);
+        }
+
+        if (!isInJail[i]) {
+            //propertiesMEH
+            xxxxxx;
+        }
+    }
+
+    public static void positionChangeCardDraw(int[] playerLocation, int i) {
+        if (cardAction.equals("move")) {
+            if (Integer.parseInt(cardChangePlayerPosition) > -1) {
+                playerLocation[i] = Integer.parseInt(cardChangePlayerPosition);
+
+                if (playerLocation[i] == 30) {
+                    goToJail(playerLocation, i);
+                }
+            } else if (Integer.parseInt(cardChangePlayerPosition) < -1) {
+                playerLocation[i] += Integer.parseInt(cardChangePlayerPosition);
+            }
+        }
+    }
+
+    public static void checkForSeriesMatchingDices(int[] playerLocation, int i) {
+        if (firstDiceThrows[i] == secondDiceThrows[i]) {
+            System.out.println(playersNames[i] + " threw a pair!");
+            countDicePairsThrownInRow[i]++;
+        } else {
+            countDicePairsThrownInRow[i] = defaultStartPosition;
+        }
+
+        if (countDicePairsThrownInRow[i] == maxTimesDiceThrows) {
+            System.out.println(playersNames[i] + " threw 3 pairs in a row and is being sent to jail.");
+            countDicePairsThrownInRow[i] = defaultStartPosition;
+            goToJail(playerLocation, i);
+        } else if (countDicePairsThrownInRow[i] < maxTimesDiceThrows && countDicePairsThrownInRow[i] >= 1) {
+            System.out.println(playersNames[i] + " can throw again.");
+        }
+    }
+
+    public static void makeTurn(int[] playerLocation) {
+        for (int i = 0; i < playersAmount; i++) {
+            if (checkWhetherAllPlayersHaveBankrupted()) {
+                System.out.println("All players have bankrupted - no winner.");
+                hasWon = true;
+                break;
+            }
+            checkBankruptedPlayer(i);
+
+            if (isBankrupted[i]) {
+                continue;
+            }
+
+            isOnlyOnePlayerLeft(i);
+
+            if (hasWon) {
+                break;
+            }
+
+            System.out.println(playersNames[i] + " 's turn: ");
+
+            for (int repeats = 0; repeats <= countDicePairsThrownInRow[i]; repeats++) {
+                if (countDicePairsThrownInRow[i] < 3) {
+                    if (!isInJail[i]) {
+                        rowTheDices(i);
+                        checkForSeriesMatchingDices(playerLocation, i);
+                        calculatePositionAfterThrownDice(playerLocation, i);
+                    }
+
+                    outputPlayerCurrentLocation(playerLocation, i);
+                    printMoney(i);
+                    decideWhatToDo(playerLocation, i);
+                }
+            }
+        }
+    }
+
+    public static void printNextTurn() {
+        System.out.println("******************************************************************************************************************");
+        System.out.println();
+
+        System.out.println("Turn: " + (++allTurns));
+    }
+
+    public static void startGame(int[] playerLocation) {
+        while (!hasWon) {
+            printNextTurn();
+            makeTurn(playerLocation);
+        }
+    }
+
+    public static void doAuction() {
+        boolean[] isPlayerNotParticipatingInTheAuction = new boolean[playersAmount];
+
+    }
+//to here
 }
